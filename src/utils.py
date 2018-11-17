392d345be6b28503d5f71c1d942cfbfa821478aa
yuxiqian <akaza_akari@sjtu.edu.cn>
2018-11-17 10:28:14 +0800

    增加徐汇校区教一楼查询。

--- src/utils.py
+++ src/utils.py
@@ -9,7 +9,7 @@ def separator():
     print("-" * os.get_terminal_size().columns)
 
 
-building_list = ["思源湖", "上院", "中院", "下院", "东上院", "东中院", "东下院"]
+building_list = ["思源湖", "上院", "中院", "下院", "东上院", "东中院", "东下院", "教一楼"]
 
 term_list = ["", "秋季学期", "春季学期", "夏季小学期"]
 
